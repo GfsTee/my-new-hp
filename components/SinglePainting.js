7915f3dd40dcd21c04a9fb73225ee5dd27f1ea66
Georg Treitz <georg.treitz@gmail.com>
2020-08-03 20:52:05 +0200

    whitespace removed

--- components/SinglePainting.js
+++ components/SinglePainting.js
@@ -1,20 +1,14 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
-
-
+import React, { useState, useRef, useEffect } from 'react';
 
 const SinglePainting2 = (props) => {
-
     const [isShown, setIsShown] = useState(false);
     const ref = useRef();
     useOnClickOutside(ref, () => setIsShown(false));
-
     return (
         <figure className="main-fig">
-
             <div className="small-img-container" onClick={() => setIsShown(true)}></div>
             <figure className={`fullscreen ${isShown ? "show" : "hide"}`} ref={ref} >
                 <div className="close">
-
                     <svg onClick={() => setIsShown(false)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.094l-4.157-4.104 4.1-4.141-1.849-1.849-4.105 4.159-4.156-4.102-1.833 1.834 4.161 4.12-4.104 4.157 1.834 1.832 4.118-4.159 4.143 4.102 1.848-1.849z" /></svg>
 
                 </div>
