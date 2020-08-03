d98e32e5087963fd9495665cbf03dd02d0c8abfe
Georg Treitz <georg.treitz@gmail.com>
2020-08-03 20:53:13 +0200

    singlepainting commented code removed

--- components/SinglePainting.js
+++ components/SinglePainting.js
@@ -10,7 +10,6 @@ const SinglePainting2 = (props) => {
             <figure className={`fullscreen ${isShown ? "show" : "hide"}`} ref={ref} >
                 <div className="close">
                     <svg onClick={() => setIsShown(false)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.094l-4.157-4.104 4.1-4.141-1.849-1.849-4.105 4.159-4.156-4.102-1.833 1.834 4.161 4.12-4.104 4.157 1.834 1.832 4.118-4.159 4.143 4.102 1.848-1.849z" /></svg>
-
                 </div>
                 <div className="big-img-container">
                     <img className="big-img" src={`${props.ele.picUrl}`} alt="" />
@@ -48,16 +47,10 @@ const SinglePainting2 = (props) => {
                 object-fit: contain;
             }
             .big-img-container {
-                // height: 100%;
                 display: flex;
                 justify-content: center;
                 align-items: center;
             }
-            // .fullscreen img {
-            //     max-width: 100%;
-            //     max-height: 100%;
-            //     object-fit: contain;
-            // }
             .hide {
                 z-index: -100000;
                 opacity: 0;
@@ -76,17 +69,12 @@ const SinglePainting2 = (props) => {
             }
             .close {
                 width: 100%;
-                // display: flex;
-                // justify-content: flex-end;
-                // padding-bottom: 20px;
-                // position: relative;
             }
             svg {
                 position: absolute;
                 right: 3px;
                 top: 3px;
                 cursor: pointer;
-                // margin: -34px -34px 0 0; 
                 fill: #F2F2F2;
             }
       `}</style>
