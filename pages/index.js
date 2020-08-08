0e3ff23c2160ddd0f8559ff82825002c1dbafa23
Georg Treitz <georg.treitz@gmail.com>
2020-08-08 20:28:27 +0200

    pictures added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout'
 import { useState, useRef, useEffect, useCallback } from 'react';
+import Link from 'next/link'
 
 export default function Home() {
   const [coords, setCoords] = useState({ x: 0, y: 0 });
@@ -18,22 +19,24 @@ export default function Home() {
     <Layout title={"Malerei"}>
       <h1>
       </h1>
-      <div className="pic">
-        <div className="pic-inverted"></div>
-      </div>
+      <Link href="/malerei">
+        <div className="pic">
+          <div className="pic-inverted"></div>
+        </div>
+      </Link>
       <div className="title">
-        aus der Serie <i>lys</i> V, Malerei auf Glas, LED, 20x30cm, 2019
+        aus der Serie <i>lys</i> IV, mit Licht, Malerei auf Glas, LED, 43x43,2 cm, 2018
       </div>
       <div className="small">
         <i>Normale Ansicht: Licht aus - wenn du die Maus über das Bild bewegst wird bei diesem Teil des Bilds die LEDs "eingeschaltet"</i>
       </div>
       <style jsx>{`
         .pic {
-          background: url(/pictures/pic2.jpg) center / contain no-repeat;
+          background: url(/pictures/2018/lys4_off.jpg) center / contain no-repeat;
           cursor: none;
         }
         .pic-inverted {
-          background: url(/pictures/pic1.jpg) center / contain no-repeat;
+          background: url(/pictures/2018/lys4_on.jpg) center / contain no-repeat;
           clip-path: circle(80px at ${coords.x}px ${coords.y}px);
           height: 80vmin;
         }
--- pages/texte.js
+++ pages/texte.js
@@ -3,7 +3,50 @@ import Layout from '../components/Layout'
 const Texte = () => {
     return (
         <Layout title={"Texte"}>
-            Texte
+            <div id="texte">
+                <h3>Die Gegenwart des Bildes und die Entschiedenheit der Frage.</h3>
+                <p>In der abstrakten Malerei von Georg Treitz wird Gegenwart erfahrbar. Er entlockt sie seinen Bildern, indem er die ruhige und konzentrierte Auseinandersetzung mit der Materialität des Mediums und der Ästhetik der Malerei zum zweipoligen Zentrum eines malerischen Feldes erhebt. Aus dieser Differenzierung entspringt das Potential einer dynamischen und produktiven, beständigen Fragehaltung. Materialität und Ästhetik greifen ineinander, verschwistern sich, um umso vehementer auseinander zu streben und in sich selbst problematisch zu werden.</p>
+                <p>Changierend zwischen Festigkeit und Geschmeidigkeit, zwischen einem kompakten und einem lasierenden Farbauftrag, zwischen matten und glänzenden Flächen, bewegtem Strich und gefüllter Fläche entsteht das Bild. Es ist mehr als die Summe seiner Teile, mehr als das Aufbringen von verschiedensten Farben und Bindemitteln auf eine zweidimensionale, rechteckige Leinwand. Jede einzelne Arbeit von Georg Treitz ist auch eine Auseinandersetzung mit ästhetischen Vorstellungen. Dabei konstruiert er keine künstlichen Grenzen, die das Werk in einer Rhetorik von Tradition und Erneuerung, zu rechtfertigen versuchen. Vielmehr wird in jedem einzelnen Werk, immer für dieses eine, das Verhältnis von Materialität und Ästhetik neu verhandelt. Zum Teil schmiegt sich der Farbauftrag ineinander, etabliert ein Zusammenspiel der Farbe und der Wahrnehmung ihrer Materialität, das in seiner Harmonie und heiteren Stimmigkeit an Deckengemälde des Barock erinnert. Zum Teil scheinen die Bilder in düstere Farben getaucht, oder irritierend durchkreuzt von Partien, die sich nicht zu einer Einheit zusammenfügen lassen.</p>
+                <p>Es etabliert sich ein ernsthaftes, das einzelne Bild übersteigendes Spiel, aufgespannt zwischen Integration und Zerrissenheit. Diese Malerei kennt kein Dogma und kein Festhalten an einem vorgängigen Ideal. Sie ist Experiment ohne Fortschrittsgläubigkeit, Prozess und Ergebnis in einem.</p>
+                <p>Denn neben die beständige Frage tritt die beständige Entscheidung. Jedes einzelne Bild besteht aus hunderten in der Vergangenheit, während seines Entstehungsprozesses getroffenen Urteilen und jedes verweist aufgrund dieser über sich selbst auf potentielle weitere Werke.</p>
+                <p>Georg Treitz Malerei ist gestaltete Auseinandersetzung und in die Falte, die Vergangenheit und Zukunft werfen, schmiegt sich die Gegenwart des einzelnen Bildes.</p>
+                <p>Und auf der anderen Seite stehen wir. Der Betrachter, der in dieser Gegenwart des Bildes sein Eigenes und damit eine andere Vergangenheit und Zukunft heranträgt, die das Werk verändert und die aufgeworfenen Fragen aktualisiert, um in diesem Akt selbst modifiziert zu werden. In ihrer Fragehaltung und Entschiedenheit sind Georg Treitz‘ Arbeiten eine wiederständige Spielwiese des Auges und des Geistes, deren Fragen präsent den Betrachter zu sehender Auseinandersetzung herausfordern.</p>
+                <p className="author"><i>Julia Gerber</i></p>
+                <div className="line"></div>
+                <h3>Text zur Ausstellung „Fetisch oder Fetischismus“ in dem Haus der Universität, Düsseldorf</h3>
+                <p>Wie eine ferne Galaxie leuchten die Bilder von Georg Treitz und ziehen uns durch ihre harmonischen Farbkompositionen und durch ihren abstrakten Formenkanon in ihren Bann. Fremd und doch vertraut, entfernt und doch nah ist uns seine Bildsprache, die über das Format hinausgehend den Betrachter in eine Welt voller Farbklänge, aber auch Farbkämpfe regelrecht hineinzieht.</p>
+                <p>Farbe bedeutet für Georg Treitz weniger „coleur“, ihn interessiert vor allem Farbe als Material, das je nach Textur seine eigenen Gesetze mit sich bringt. Wie agieren und wie reagieren Farben? Welche chemischen und physikalischen Prozesse entstehen, wenn sich unterschiedliche Farbtypen auf einem Bildträger begegnen, sich berühren, überlagern und doch gleichzeitig voneinander wegdrängen? Dynamisch und kraftvoll wirken die Kompositionen, deren Anziehungskraft sich nicht nur aus dem Duktus des Künstlers speist, sondern auch aus dem Umgang und Experimentieren mit verschiedenen Farbtypen. Lasierend oder deckend, matt oder glänzend, schroff und fordernd oder sanft und zart begegnen uns diese Farbenspiele, die zwar auf einem zweidimensionalen Bildträger aufgetragen wurden, uns aber durch ihre Kraft und ihr Pathos umhüllen und ins Bildinnere ziehen, bisweilen geradezu in soghafter Manier.</p>
+                <p>Bei Georg Treitz trifft der Begriff Materialfetisch wohl zu wie bei keinem anderen. Sein Fetischismus basiert auf einem symbolischen Zusammenspiel unterschiedlicher Farbtypen und der Faszination des Experiments. Wie unterschiedlich kann Farbe als Substanz auf einem Malgrund miteinander harmonieren bei gleichzeitig konträren Positionen? Durch seinen spielerischen Umgang mit verschiedenen Maltechniken setzt Georg Treitz zusätzlich dezidiert Akzente in seinen Bildern, lotet aus, geht dabei bis an die Grenzen des Möglichen. Somit verleiht er seinen Werken eine rätselhafte Aura. Bunsenbrenner, Klarsicht- und Alufolie, Spachteln und Spritzen dienen ihm dabei als technische Hilfsmittel.</p>
+                <p>Die Dominanz der Farbe Blau in den Bildern des Künstlers ist unübersehbar. Bravourös, in vielfältigen Nuancen maltechnisch umgesetzt, beherrscht sie in weiten Teilen den gesamten Bildraum. Mal mutiert sie zu dem Element Wasser, lässt assoziieren auf vielfältige Weise zu Naturgewalt, Strudel, Gischt, bis hin zu weichen wellenartigen Verläufen und kulminiert so zu einer ganz eigenen Ästhetik.</p>
+                <p>In anderen Werken wiederum fetischiert Georg Treitz die Malerei und nutzt sie zu Bildaussagen, die an kartografierte vulkanische Landschaften erinnern.</p>
+                <p>Malerei ist bei Treitz gestaltete, permanente Auseinandersetzung, die in ihrer ästhetischen formalen und inhaltlichen Variabilität den Betrachter zum Dialog mit dem jeweiligen Werk herausfordert mit der nachhaltigen Kraft einer sinnlich erfahrbaren Faszination.</p>
+                <p className="author"><i>Anna-Lena Rößner</i></p>
+            </div>
+
+            <style jsx>{`
+                #texte {
+                    width: 60%;
+                    margin: 0 auto;
+                    text-align: justify;
+                }
+                h3 {
+                    padding: .5rem 0;
+                    color: #172626;
+                }
+                p{
+                    padding: .3rem 0;
+                }
+                .author {
+                    text-align: right;
+                    padding: 0;
+                }
+                .line {
+                    height: 1px;
+                    background: #97A4A6;
+                    margin: 3rem auto;
+                    width: 96%;
+                }
+            `}</style>
         </Layout>
     );
 }
